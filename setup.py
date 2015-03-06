df46487d63a518803f98dfc5d514a7c6d34f58e1
Thomas Levine <_@thomaslevine.com>
2015-03-06 21:28:51 +0000

    package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='immaterial-digital-labor',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Stuff related to the Immaterial Digital Labor group',
+      url='http://thomaslevine.com',
+      install_requires = ['vlermv', 'requests'],
+      tests_require = ['nose'],
+      packages = ['archive'],
+      entry_points={'console_scripts': ['immaterial-digital-labor-archive = archive:cli']},
+      version='0.1',
+      license='AGPL',
+      classifiers=[
+          'Programming Language :: Python :: 3.4',
+      ],
+)
