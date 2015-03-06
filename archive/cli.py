cd1aa8376d467f7ce64510db24932e540a85af7e
Thomas Levine <_@thomaslevine.com>
2015-03-06 21:27:15 +0000

    -

--- archive/cli.py
+++ archive/cli.py
@@ -1,3 +1,33 @@
 import argparse
+from .download import download
+from .condense import condense
 
-argparse.ArgumentParser(description = 'Download links from the Immaterial Digital Labor group, and present them in some presentable way.')
+ACCESS_TOKEN_MESSAGE = '''
+Go to this address, get an access token, and paste it below.
+https://developers.facebook.com/tools/explorer/
+
+'''
+FIELDNAMES = [
+    'message',
+    'facebook-link', 'external-link',
+    'from-id', 'from-name',
+]
+
+def parse_args():
+    argparser = argparse.ArgumentParser(
+        description = 'Download links from the Immaterial Digital Labor group, '\
+                      'and present them in some presentable way.',
+    )
+    argparser.add_argument('--access_token')
+
+    _args = argparse.parse_args()
+    if None == _args.access_taken:
+        _args.access_token = input(ACCESS_TOKEN_MESSAGE)
+    return _args
+
+def main():
+    import csv, sys
+    args()
+    w = csv.DictWriter(sys.stdout, FIELDNAMES):
+    for post in download(access_token):
+        w.writerows(condense(post))
--- archive/condense.py
+++ archive/condense.py
@@ -1,13 +1,25 @@
 def condense(post):
     fb_link = post['actions'][0]['link']
-    yield {
+
+    x = {
         'message': post['message'],
-        'facebook-link': link,
+        'facebook-link': fb_link,
         'external-link': post.get('link')
     }
+    x.update(_parse_from(post))
+    yield x
+
     for comment in post.get('comments', {}).get('data', []):
-        yield {
+        x = {
             'message': comment['message'],
-            'facebook-link': link,
+            'facebook-link': fb_link,
             'external-link': None,
         }
+        x.update(_parse_from(comment))
+        yield x
+
+def _parse_from(datum):
+    return {
+        'from-id': datum['from']['id'],
+        'from-name': datum['from']['name'],
+    }
